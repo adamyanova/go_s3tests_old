5018749ae5867e9f93766e59484543f0836efa7e
Antoaneta Damyanova <adamyanova@gmail.com>
2018-05-30 12:13:07 +0200

    golang install added

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,7 +16,7 @@ if [ -f /etc/debian_version ]; then
         sudo apt-get -y install $missing
     fi
 elif [ -f /etc/fedora-release ]; then
-    for package in golang-gopkg-yaml-devel-v2; do
+    for package in golang-gopkg-yaml-devel-v2 golang; do
         if [ "$(rpm -qa $package 2>/dev/null)" == "" ]; then
             missing="${missing:+$missing }$package"
         fi
@@ -26,7 +26,7 @@ elif [ -f /etc/fedora-release ]; then
         sudo yum -y install $missing
     fi
 elif [ -f /etc/redhat-release ]; then
-    for package in golang-gopkg-yaml-devel-v2; do
+    for package in golang-gopkg-yaml-devel-v2 golang; do
         if [ "$(rpm -qa $package 2>/dev/null)" == "" ]; then
             missing="${missing:+$missing }$package"
         fi
